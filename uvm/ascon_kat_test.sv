2f14025d019dbcaae8a0bde6a81f5042d97abd37
Athanasios Krikis <73754661+TommyKrikis@users.noreply.github.com>
2026-08-26 01:58:57 +0300

    feat: KAT sequence driving all 1089 NIST reference vectors

---
+++ uvm/ascon_kat_test.sv
@@ -0,0 +1,26 @@
+class ascon_kat_test extends uvm_test;
+  ascon_env my_env;
+  ascon_kat_sequence my_seq;
+  `uvm_component_utils(ascon_kat_test)
+
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    uvm_top.set_timeout(2ms, 0);
+    my_env      = ascon_env::type_id::create("my_env", this);
+    
+   endfunction
+
+  task run_phase(uvm_phase phase);
+    my_seq = ascon_kat_sequence::type_id::create("my_seq");
+    my_seq.num_vectors = 1089;
+    phase.phase_done.set_drain_time(this, 100ns);
+    phase.raise_objection(this);
+    `uvm_info("BASE_TEST", "UVM flow is alive", UVM_LOW)
+    my_seq.start(my_env.agt.seqr);
+    phase.drop_objection(this);
+  endtask
+endclass
